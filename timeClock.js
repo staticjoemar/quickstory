fdfd88bbc2888bd28939ecb77e894fc3c43db6d8
staticjoemar <156019350+staticjoemar@users.noreply.github.com>
2024-04-26 07:15:45 -1000

    Add files via upload

--- timeClock.js
+++ timeClock.js
@@ -1,7 +1,6 @@
 
 
 let login = sessionStorage.getItem("login");
-
 if((login=="null")||(login==null)){
     document.getElementById("main").setAttribute("hidden","hidden");
     document.getElementById("login").removeAttribute("hidden");
@@ -10,7 +9,6 @@ else{
     document.getElementById("login").setAttribute("hidden","hidden");
     document.getElementById("main").removeAttribute("hidden");
 }
-
 document.getElementById("loginBtn").onclick=function(){
     let a = document.getElementById("x").value;
     let b = document.getElementById("y").value;
@@ -57,80 +55,65 @@ document.getElementById("loginBtn").onclick=function(){
     else {
         document.getElementById("failed").textContent = "INVALID LOGIN"
     }}
-
-
-let sec = 0;
+let sec =0;
+let hr = 0;
 let count =0;
 total = 0; 
 let init = +localStorage.getItem("init");
-
 document.getElementById("timeOutBtn").onclick=function(){
+    //toggle button
     document.getElementById("timerActive").removeAttribute("hidden");
-
-
-
     document.getElementById("timeInBtn").removeAttribute("disabled");
     document.getElementById("timeInBtn").style.backgroundColor="darkslategray";
     document.getElementById("timeOutBtn").setAttribute("disabled", "disabled");
     document.getElementById("timeOutBtn").style.backgroundColor="gray";
-
-
+    //get start reference
     let x = new Date();
     let y = x.getTime();
     localStorage.setItem("startTime",y);
 }
 document.getElementById("timeInBtn").onclick=function(){
+    //toggle button
     document.getElementById("timerActive").setAttribute("hidden","hidden");
-    
-
-
-
-
     document.getElementById("timeOutBtn").removeAttribute("disabled");
     document.getElementById("timeOutBtn").style.backgroundColor="darkslategray";
     document.getElementById("timeInBtn").setAttribute("disabled", "disabled");
     document.getElementById("timeInBtn").style.backgroundColor="gray";
-
-    document.getElementById("day").innerHTML = sec;
-
+    //print split
+    document.getElementById("day").innerHTML = hr;
     splitTime();
-    dayTime();
-    
+    dayTime();   
 }
-
-
-
 function splitTime(){
-    
+    //for split saves
     count++;
+    //get end reference
     let x = new Date();
     let y = x.getTime();
+    //declare variables to calculate split time
     let z = +localStorage.getItem("startTime");
+    //a = split in seconds
     let a = (y - z)/1000;
-    sec = a/60;
-    localStorage.setItem("splitTime"+count, sec);
+    sec = a.toFixed(2);
+    //convert to hours
+    hr = (a/3600).toFixed(3);
+    // save split time as new int
+    localStorage.setItem("splitTime"+count, hr);
+    //save new total 
     localStorage.setItem("init", init);
-   document.getElementById("day").textContent = sec;
+    //print
+   document.getElementById("day").textContent = hr;
+   document.getElementById("sec").innerHTML = "SECONDS: " + sec;
 }
-
 document.getElementById("deletestorage").onclick=function(){
     localStorage.clear();
 }
-
 function dayTime(){
-
 let x = +localStorage.getItem("init");
 let y = +localStorage.getItem("splitTime"+count);
  init = x+y;
 localStorage.setItem("init",init);
-document.getElementById("totalpd").innerHTML = init;
-
-
- 
-
-
-
-
+document.getElementById("totalpd").innerHTML = init.toFixed(3);
 }
 
 
