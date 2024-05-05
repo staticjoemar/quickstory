3235cc7deade2c0c244b93f2e29ac29ae45f0401
staticjoemar <156019350+staticjoemar@users.noreply.github.com>
2024-05-05 11:56:07 -1000

    Add files via upload

--- timeClock.js
+++ timeClock.js
@@ -31,7 +31,9 @@ document.getElementById("loginBtn").onclick=function(){
     if((a == "devtest")&&(b=="tester188")){
         document.getElementById("main").removeAttribute("hidden");
         document.getElementById("login").setAttribute("hidden","hidden");
-        sessionStorage.setItem("login", "xyz");}
+        sessionStorage.setItem("login", "LoginSuccess");
+        localStorage.setItem("currentUser", a)}
+        currentUser=a;
     if((a == "dev32492u3njfdsnt")&&(b=="teste092342knsdo")){
         document.getElementById("main").removeAttribute("hidden");
         document.getElementById("login").setAttribute("hidden","hidden");
@@ -44,10 +46,12 @@ document.getElementById("loginBtn").onclick=function(){
         document.getElementById("main").removeAttribute("hidden");
         document.getElementById("login").setAttribute("hidden","hidden");
         sessionStorage.setItem("login", "xyz");}
-    if((a == "dev32492nkjfdsnt")&&(b=="teste0923j42knsdo")){
+    if((a == "188")&&(b=="188")){
         document.getElementById("main").removeAttribute("hidden");
         document.getElementById("login").setAttribute("hidden","hidden");
-        sessionStorage.setItem("login", "xyz");}
+        sessionStorage.setItem("login", "LoginSuccess");
+        localStorage.setItem("currentUser", a)
+        currentUser=a}
     if((a == "de48283492v32492u3nkjfdsnt")&&(b=="teste423v0923jknsdo")){
         document.getElementById("main").removeAttribute("hidden");
         document.getElementById("login").setAttribute("hidden","hidden");
@@ -60,6 +64,34 @@ let hr = 0;
 let count =0;
 total = 0; 
 let init = +localStorage.getItem("init");
+let currentUser = localStorage.getItem("currentUser");
+
+
+
+
+//set button status on refresh or tab change
+if (localStorage.getItem("clockedOut")=="clockedOut"){
+    document.getElementById("timerActive").removeAttribute("hidden");
+    document.getElementById("timeInBtn").removeAttribute("disabled");
+    document.getElementById("timeInBtn").style.backgroundColor="darkslategray";
+    document.getElementById("timeOutBtn").setAttribute("disabled", "disabled");
+    document.getElementById("timeOutBtn").style.backgroundColor="gray";
+
+}
+if (localStorage.getItem("clockedOut")=="null"){
+    document.getElementById("timerActive").setAttribute("hidden","hidden");
+    document.getElementById("timeOutBtn").removeAttribute("disabled");
+    document.getElementById("timeOutBtn").style.backgroundColor="darkslategray";
+    document.getElementById("timeInBtn").setAttribute("disabled", "disabled");
+    document.getElementById("timeInBtn").style.backgroundColor="gray";
+    
+
+}
+
+//set total pd on refresh or tab change
+
+document.getElementById("totalpd").innerHTML="TECH "+currentUser+": "+init.toFixed(3);
+//
 document.getElementById("timeOutBtn").onclick=function(){
     //toggle button
     document.getElementById("timerActive").removeAttribute("hidden");
@@ -71,6 +103,8 @@ document.getElementById("timeOutBtn").onclick=function(){
     let x = new Date();
     let y = x.getTime();
     localStorage.setItem("startTime",y);
+    //change clocked out state
+    localStorage.setItem("clockedOut", "clockedOut");
 }
 document.getElementById("timeInBtn").onclick=function(){
     //toggle button
@@ -83,6 +117,8 @@ document.getElementById("timeInBtn").onclick=function(){
     document.getElementById("day").innerHTML = hr;
     splitTime();
     dayTime();   
+    // changed clocked out state
+    localStorage.setItem("clockedOut","null");
 }
 function splitTime(){
     //for split saves
@@ -105,15 +141,19 @@ function splitTime(){
    document.getElementById("day").textContent = hr;
    document.getElementById("sec").innerHTML = "SECONDS: " + sec;
 }
+//DEBUG
 document.getElementById("deletestorage").onclick=function(){
     localStorage.clear();
+}
+document.getElementById("deleteSessionStorage").onclick=function(){
+    sessionStorage.clear();
 }
 function dayTime(){
 let x = +localStorage.getItem("init");
 let y = +localStorage.getItem("splitTime"+count);
  init = x+y;
 localStorage.setItem("init",init);
-document.getElementById("totalpd").innerHTML = init.toFixed(3);
+document.getElementById("totalpd").innerHTML ="TECH "+currentUser+": "+ init.toFixed(3);
 }
 
 
